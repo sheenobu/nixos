b58cedf3d07b96f4d858b04fb6f12394d93c22e4
Sheena Artrip <sheena.artrip@gmail.com>
2016-08-20 18:16:27 -0400

    rabbit - added boot config

---
+++ machines/rabbit/boot.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, lib, ... }:
+{
+  boot = {
+    blacklistedKernelModules = [ "r8169" ];
+    extraModulePackages = [
+     (pkgs.sheenobupkgs.r8168 { kernel = pkgs.linux; })
+    ];
+    kernelPackages = pkgs.linuxPackages;
+    kernelParams = [ "ipv6.disable=1" ]; # "pcie_aspm=off" ];
+    loader.gummiboot.enable=true;
+  };
+}
