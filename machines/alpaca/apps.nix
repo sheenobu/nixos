4db3becc485aaf5256a583af57ada36ce73a1c8f
Sheena Artrip <sheena.artrip@gmail.com>
2016-08-31 20:35:42 -0400

    alpaca - fix asterisk, install keepassx and asterisk

--- machines/alpaca/apps.nix
+++ machines/alpaca/apps.nix
@@ -23,11 +23,28 @@
 
         # social
         slack
+
+        # sec
+        keepassx
+
+        #
+        asterisk
     ];
 
     programs.tmux = {
       enable = true;
       extraTmuxConf = builtins.readFile ./tmux/config;
+    };
+
+    nixpkgs.config.packageOverrides = pkgs: {
+      asterisk = pkgs.stdenv.lib.overrideDerivation pkgs.asterisk  (oldAttrs: rec {
+        version = "13.10.0";
+        name = "asterisk-${version}";
+        src = pkgs.fetchurl {
+            url = "http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${version}.tar.gz";
+            sha256 = "1cqkjm3bq0rslyka5bjqf89iwgs8sqd2l6hcx8dcp9kz0q0mv95b";
+        };
+      });
     };
 
 }
