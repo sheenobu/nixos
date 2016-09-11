c313c1ec85031bb20589b09389dbb64602132e40
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-11 19:47:12 -0400

    testing - add test lemur system

---
+++ test/test-lemur.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ../modules
+    ../modules/backports.nix # 16.03
+
+    ../machines/lemur/base.nix
+    ../machines/lemur/desktop.nix
+    ../machines/lemur/apps.nix
+  ];
+
+  virtualisation.memorySize = 1024;
+  virtualisation.qemu.options = [ "-full-screen -sdl" ];
+
+  # disable clamav so we don't try to update the virus DB every time we boot a new system
+  services.clamav.updater.enable = false;
+
+  users.users.root.password = "test";
+  users.users.sheenobu.password = "test";
+
+  networking.hostName = "lemur-test";
+
+  system.stateVersion = "16.03";
+}
