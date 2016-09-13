ed6ecb07dc01ae7922b71b19b6e425946c42ed4f
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-13 04:36:01 -0400

    test - make autologin-bug box use spice

--- test/test-autologin-bug.nix
+++ test/test-autologin-bug.nix
@@ -2,16 +2,17 @@
 {
 
   services.xserver.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.default = "i3"; # must specify default if auto.enable is on
+  services.xserver.windowManager.openbox.enable = true;
+  services.xserver.windowManager.default = "openbox"; # must specify default if auto.enable is on
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.user = "root";
+
   services.xserver.resolutions = { x = 1680; y = 1050; };
   services.xserver.virtualScreen = { x = 1680; y = 1050; };
 
   virtualisation.memorySize = 1024;
-  virtualisation.qemu.options = [ "" ];
+  virtualisation.qemu.options = [ "-spice port=6665" ];
 
   users.users.root.password = "test";
   networking.hostName = "basic-test";
