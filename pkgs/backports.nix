6027e6b482bf943b76fb3d3e60f87603feed160c
Sheena Artrip <sheena.artrip@gmail.com>
2016-10-17 09:52:49 -0400

    remove 16.09 specific backport code

--- pkgs/backports.nix
+++ pkgs/backports.nix
@@ -2,15 +2,6 @@
 
 let
 
-  # backport to 16.03, for makeSearchPathOutput
-  getOutput = output: pkg:
-    if pkg.outputUnspecified or false
-      then pkg.${output} or pkg.out or pkg
-      else pkg;
-
-  # backport to 16.03
-  makeSearchPathOutput = output: subDir: pkgs: lib.makeSearchPath subDir (map (getOutput output) pkgs);
-
   # let's define our own callPackage to avoid typing all dependencies
   callPackage = pkgs.lib.callPackageWith (pkgs // backports);
 
@@ -28,17 +19,6 @@ let
 
     # backport to 16.03
     mednaffe = (pkgs.callPackage ./mednaffe) {};
-
-    # backport to 16.03
-    atomEnv = (pkgs.callPackage ./vscode/atom-env.nix) {
-      gconf = pkgs.gnome.GConf;
-      makeSearchPathOutput = makeSearchPathOutput;
-    };
-
-    # backport to 16.03
-    vscode = (pkgs.callPackage ./vscode) {
-      atomEnv = atomEnv;
-    };
 
     # backport to 16.03
     gosu = (pkgs.callPackage ./gosu) {};
@@ -60,7 +40,6 @@ let
 
     # backport to 16.03
     i3-gaps = (pkgs.callPackage ./i3/gaps.nix) {};
-
 
   };
 in pkgs // {
